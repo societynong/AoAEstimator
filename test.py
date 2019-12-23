5196ef04a0cf70447d33eaf60d1e1a07cb84372e
societynong <172065208@qq.com>
2019-12-23 11:12:29 +0800

    系统兼容性修复

--- test.py
+++ test.py
@@ -10,16 +10,20 @@ from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import pandas as pd
+import platform
 
 MATPATH = 'AoAmat'
 PKLPATH = 'AoApkl'
 RESPATH = 'AoAres'
+COFPATH = 'AoApng'
 SHAPE = ['Square','ULA']
-TYPE = ['GAmp','UAmp']
+TYPE = ['GAmp','UAmp','300GAmp','1000GAmp']
 NUM = [2,3,4]
-SNR = ['5Pow']
+SNR = ['5Pow','INFPow']
 dc = {'GAmp':'Data',
-      'UAmp':'Data'}
+      'UAmp':'Data',
+      '300GAmp':'Data',
+      '1000GAmp':'Data',}
     
 def getData():
     for sp in SHAPE:
@@ -47,7 +51,7 @@ def getData():
                         with open(pklPath,'wb') as f:
                             pkl.dump((X,y),f)
 
-def testData(filename,tUsed = None):
+def testData(filename,tUsed = None,typeUsed = None):
     pklLoad = '{}/{}.pkl'.format(PKLPATH,filename)
     with open(pklLoad,'rb') as f:
             X,y = pkl.load(f)
@@ -55,11 +59,15 @@ def testData(filename,tUsed = None):
     # model = MLPClassifier()
     if tUsed is None:
         tUsed = X.shape[2]
-    pklSave = '{}/{}_{}.pkl'.format(RESPATH,filename,tUsed)
+    if typeUsed is None:
+        pklSave = '{}/{}_{}.pkl'.format(RESPATH,filename,tUsed)
+    else:
+        pklSave = '{}/{}_{}_{}.pkl'.format(RESPATH,filename,tUsed,typeUsed)
     if not os.path.exists(pklSave):
         print('X.shape:{}'.format(X.shape))
-        
         X = X[:,:,:tUsed]
+        if typeUsed is not None:
+            X = X[:,:,:,typeUsed]
         # X = transformT(X,t1)
         X = X.reshape(X.shape[0],-1)    
         # # X = np.concatenate((X[:,200:400] - X[:,:200],X[:,400:] - X[:,200:400]),1)
@@ -90,11 +98,20 @@ import sklearn
 
 
 def generateFrame():
-    rec = {'Shape':[],'Type':[],'Num':[],'S/N':[],'Time':[],'Acc(-8 to +8)':[]}
+    rec = {'Shape':[],'Type':[],'Num':[],'S/N':[],'Time':[],'Dim':[],'Acc(-8 to +8)':[]}
     
     for file in os.listdir(RESPATH):
         filename,_ = file.split('.')
-        sp,tp,nm,s2n,ti = filename.split('_') 
+        keys = filename.split('_') 
+        if len(keys) == 5:
+            sp,tp,nm,s2n,ti = filename.split('_') 
+            rec['Dim'].append('all')
+        elif len(keys) == 6:
+            sp,tp,nm,s2n,ti,dim = filename.split('_') 
+            rec['Dim'].append(dim)
+        else:
+            return 
+
         acc = evalueData(filename)
         rec['Shape'].append(sp)
         rec['Type'].append(tp)
@@ -102,8 +119,9 @@ def generateFrame():
         rec['S/N'].append(s2n)
         rec['Time'].append(ti)
         rec['Acc(-8 to +8)'].append('{:.2%}'.format(acc))
+
     df = pd.DataFrame(rec) 
-    df = df.sort_values(by=['Shape','Num','Time'])
+    df = df.sort_values(by=['Shape','Num','S/N','Time'])
     
     mdData = (csv2md(df.to_csv(index=False)))
     print(mdData)
@@ -111,7 +129,13 @@ def generateFrame():
 
 def csv2md(csvData):
     ret = ''
-    lines = csvData.split('\n')
+    if platform.system() == 'Linux':
+        spliter = '\n'
+    elif platform.system() == 'Windows':
+        spliter = '\r\n'
+    else:
+        return 
+    lines = csvData.split(spliter)
     for i,line in enumerate(lines):
         if len(line) == 0 :
             break
@@ -130,17 +154,45 @@ def csv2md(csvData):
 
     return ret
 
+def showConfuseMat(filename):
+    pklLoad = '{}/{}.pkl'.format(RESPATH,filename)
+    with open(pklLoad,'rb') as f:
+        y_pred,y_test = pkl.load(f)
+    # print(confusion_matrix(y_pred,y_test))
+    plt.figure()
+    plt.imshow(confusion_matrix(y_pred,y_test))
+    plt.savefig('{}/{}.png'.format(COFPATH,filename))
+    plt.show()
+    # return np.mean(np.abs(y_pred - y_test) <= 8)
 
 if __name__ == '__main__':
+    # plt.show()
+    # showConfuseMat('Square_GAmp_4_INFPow_5')
     getData()
-    # n = 4
+    # # n = 4
 
-    testData('Square_GAmp_4_5Pow')
-    # tobetest = ['Square_UAmp_4_5Pow','ULA_UAmp_2_5Pow','ULA_UAmp_3_5Pow','ULA_UAmp_4_5Pow']
-    # ts = [1,3,5]
+    tobetest = ['Square_300GAmp_4_5Pow','Square_1000GAmp_4_5Pow']
+    for tobe in tobetest:
+       for i in range(2): 
+        testData(tobe,typeUsed=i)
+    # tobetest = os.listdir(PKLPATH)
+    # ts = [5]
     # for fn in tobetest:
     #     for t in ts:
-    #         testData(fn,t)
-    # evalueData('ULA_UAmp_4_5Pow',1)
-    # print(sklearn.__version__)
+    #         f = fn.split('.')
+    #         testData(f[0],t)
+    # # evalueData('ULA_UAmp_4_5Pow',1)
+    # # print(sklearn.__version__)
     generateFrame()
+
+    # testData('Square_GAmp_4_5Pow',typeUsed=1)
+    # print(evalueData('Square_GAmp_4_5Pow_5'))
+    # showConfuseMat('Square_300GAmp_4_5Pow_5')
+    # pklLoad = '{}/{}.pkl'.format(RESPATH,'Square_300GAmp_4_5Pow_5')
+    # with open(pklLoad,'rb') as f:
+    #     y_pred,y_test = pkl.load(f)
+
+    # mat = confusion_matrix(y_pred,y_test)
+    # plt.figure()
+    # plt.plot(mat[:,2])
+    # plt.show()
